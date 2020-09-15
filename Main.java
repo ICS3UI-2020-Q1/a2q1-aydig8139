0e1cb6bb11be4c4bcca68d31de6ea85cb582be07
aydig8139 <aydig8139@wrdsb.ca>
2020-09-15 17:01:18 +0000

    completed code to determine if a number is even or odd

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
+
 /**
- *
- * @author 
+ * Tells the user whether the number input is even or odd.
+ * @author Rose
  */
 public class Main {
 
@@ -9,7 +11,28 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // create a Scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    // constant for the divisbility number
+    final int DIVISIBILITY = 2;
+
+    // constant for the even number remainder
+    final int REMAINDER_EVEN = 0;
+
+    // constant for the odd number remainder
+    final int REMAINDER_ODD = 1;
+
+    // Ask the user for a number
+    System.out.println("Please enter an integer:");
+    int integer = input.nextInt();
+
+    // Check to see if the number is even or odd
+    if (integer % DIVISIBILITY == REMAINDER_EVEN) {
+      System.out.println( integer + " is an even number");
+    } else if (integer % DIVISIBILITY == REMAINDER_ODD) {
+      System.out.println( integer + " is an odd number");
+    }
     
   }
 }
